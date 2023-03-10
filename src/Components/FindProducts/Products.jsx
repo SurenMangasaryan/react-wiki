1b904dd3543b5ac13073fab30a8e3bb239d1b8a6
SurenMangasaryan <suromangasaryan@gmail.com>
2023-03-10 20:53:09 +0400

    react-wiki

--- src/Components/FindProducts/Products.jsx
+++ src/Components/FindProducts/Products.jsx
@@ -5,16 +5,18 @@ import '../FindProducts/Products.css'
 export default function Products() {
 
     const prodVal = useContext(MyContext);
-
+    console.log(prodVal.findArr);
     return (
         <div className='cart'>
             {prodVal.findArr !== undefined ?
                 prodVal.findArr.map(item => {
                     return (
-                        <div key={item.pageid} className="cart-box">
-                            <h3 className='h3'>{item.title}</h3>
-                            <p className='p'>{item.snippet}</p>
-                        </div>
+                        <a href={`https://en.wikipedia.org/?curid=${item.pageid}`} key={item.pageid}>
+                            <div className="cart-box">
+                                <h3 className='h3'>{item.title}</h3>
+                                <p className='p'>{item.snippet}</p>
+                            </div>
+                        </a>
                     )
                 })
                 : null
